756deedfbbd628a696a82bd5dce3dd7e61cb3ede
emyls <emyledirisooriyastudent@gmail.com>
2019-09-25 00:15:54 +0530

    add changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var axios = require('axios');
+const dotenv = require('dotenv');
+dotenv.config();
+
+var home_path = path.join(__dirname, '..', 'public', 'templates', 'home.html');
+
+var FormData = require('form-data');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.sendFile(home_path);
 });
+
+router.get('/getCode', function(req, res, next) {
+
+  var url = 'https://slack.com/api/oauth.access?code='+req.query.code
+      +'&client_id='+process.env.client_id
+      +'&client_secret='+process.env.client_secret
+      +'&redirect_url=http://localhost:3000/getCode';
+
+  axios.get(url)
+      .then(function (resp) {
+
+        console.log(resp.data.access_token);
+        res.redirect(`/?access_token=${resp.data.access_token}`);
+
+      })
+      .catch(function (error) {
+        res.send(error);
+      });
+});
+
 
 module.exports = router;
